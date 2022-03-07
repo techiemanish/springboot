89f6478ffb2c71e287cf418821e59a4b17cc45b7
techiemanish <manishtiwari627@gmail.com>
2022-03-07 14:42:36 +0530

    Unidirection Mapping Implemented

---
+++ src/main/java/com/api/book/restapibook/Entity/Author.java
@@ -0,0 +1,41 @@
+package com.api.book.restapibook.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+    @Id
+    // @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String author_name;
+    private String author_language;
+    
+    public Author(int id, String author_name, String author_language) {
+        this.id = id;
+        this.author_name = author_name;
+        this.author_language = author_language;
+    }
+    public Author() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAuthor_name() {
+        return author_name;
+    }
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+    public String getAuthor_language() {
+        return author_language;
+    }
+    public void setAuthor_language(String author_language) {
+        this.author_language = author_language;
+    }
+}
--- src/main/java/com/api/book/restapibook/Entity/Book.java
+++ src/main/java/com/api/book/restapibook/Entity/Book.java
@@ -1,59 +1,47 @@
 package com.api.book.restapibook.Entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "books")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    // @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String title;
-    private String author;
-    
-    public Book(int id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
 
-    public Book() {
-    }
-
+    @OneToOne(cascade = CascadeType.ALL)
+    private Author author;
     public int getId() {
         return id;
     }
-
+    public Book() {
+    }
     public void setId(int id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
-
-   
-
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
-
     @Override
     public String toString() {
         return "Book [author=" + author + ", id=" + id + ", title=" + title + "]";
     }
-
+    
     
 }

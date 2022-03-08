572739edd61bb85068baa05e1795ab0aae3f2a6e
techiemanish <manishtiwari627@gmail.com>
2022-03-08 11:29:40 +0530

    File upload API endpoint successfully implemented.

--- src/main/java/com/api/book/restapibook/Controllers/FileUploadController.java
+++ src/main/java/com/api/book/restapibook/Controllers/FileUploadController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class FileUploadController {
@@ -23,7 +24,8 @@ public class FileUploadController {
             }
             boolean result = fileUploadHelper.uploadFile(file);
             if(result){
-                return ResponseEntity.ok("File is successfully uploaded.");
+                // return ResponseEntity.ok("File is successfully uploaded.");
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/images/").path(file.getOriginalFilename()).toUriString());
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/api/book/restapibook/helper/FileUploadHelper.java
+++ src/main/java/com/api/book/restapibook/helper/FileUploadHelper.java
@@ -2,16 +2,24 @@ package com.api.book.restapibook.helper;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 @Component
 public class FileUploadHelper {
-    public final String upload_dir = "C:\\Users\\manis\\OneDrive\\Desktop\\SpringBootProject\\rest-api-book\\src\\main\\resources\\static\\images";
+    //public final String upload_dir = "C:\\Users\\manis\\OneDrive\\Desktop\\SpringBootProject\\rest-api-book\\src\\main\\resources\\static\\images";
+    public final String upload_dir = new ClassPathResource("static/images/").getFile().getAbsolutePath();
+    
+
+    public FileUploadHelper() throws IOException {
+    }
+
 
     public boolean uploadFile(MultipartFile file){
         boolean flag = false;
